117d58a23874056ffeffc967ec25cebba71ac404
Kyle Spencer <spencerk2@vcu.edu>
2019-01-28 02:12:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+  new_string = []
+  
+  string_to_array = string.split(" ")
+  
+  string_to_array.each do 
+    |x| new_string << x.reverse 
+            end
+  
+  new_string.join(" ")
+  
+end
